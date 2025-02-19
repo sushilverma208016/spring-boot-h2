e85036d97a58392fb72bc193e229413d171dbc61
sushil <sushil@sushils-MacBook-Air.local>
2025-02-19 06:16:17 +0530

    second commit

--- src/main/java/com/sushil/spring/h2/controller/TutorialController.java
+++ src/main/java/com/sushil/spring/h2/controller/TutorialController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sushil.spring.h2.model.Tutorial;
 import com.sushil.spring.h2.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
